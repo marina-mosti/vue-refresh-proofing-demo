3aa0d2840f567c62775f4ca37d1b3489c5a229c0
Marina Mosti <marina@mosti.com.mx>
2019-11-19 09:08:01 -0600

    Wrap up example

--- src/App.vue
+++ src/App.vue
@@ -4,27 +4,27 @@
     <hr />
     <form>
       <label for="firstName">First Name</label>
-      <input type="text" id="firstName" v-model="firstName" />
+      <input type="text" id="firstName" @input="updateForm('firstName', $event.target.value)" :value="form.firstName" />
 
       <label for="lastName">Last Name</label>
-      <input type="text" id="lastName" v-model="lastName" />
+      <input type="text" id="lastName" @input="updateForm('lastName', $event.target.value)" :value="form.lastName" />
 
       <label for="email">Email</label>
-      <input type="email" id="email" v-model="email" />
+      <input type="email" id="email" @input="updateForm('email', $event.target.value)" :value="form.email" />
 
       <label for="framework">Favorite Framework</label>
-      <select id="framework" v-model="framework">
-        <option value="vue">Vue</option>
-        <option value="stillvue">Vue!</option>
-        <option value="vuevuevue">Vue :D</option>
-        <option value="okfine">Other</option>
+      <select id="framework" @change="updateForm('framework', $event.target.value)">
+        <option value="vue" :selected="form.framework === 'vue'">Vue</option>
+        <option value="stillvue" :selected="form.framework === 'stillvue'">Vue!</option>
+        <option value="vuevuevue" :selected="form.framework === 'vuevuevue'">Vue :D</option>
+        <option value="okfine" :selected="form.framework === 'okfine'">Other</option>
       </select>
 
       <label for="extras">Heres an extra space to describe why you love Vue</label>
-      <textarea id="extras" v-model="extras" rows="5"></textarea>
+      <textarea id="extras" @input="updateForm('extras', $event.target.value)" :value="form.extras" rows="5"></textarea>
 
       <label>
-        <input type="checkbox" v-model="spam" />
+        <input type="checkbox" :checked="form.spam" @change="updateForm('spam', $event.target.checked)" />
         I want all the spams
       </label>
 
@@ -38,12 +38,37 @@ export default {
   name: 'app',
   data () {
     return {
-      firstName: '',
-      lastName: '',
-      email: '',
-      framework: 'vue',
-      extras: '',
-      spam: false
+      form: {
+        firstName: '',
+        lastName: '',
+        email: '',
+        framework: 'vue',
+        extras: '',
+        spam: false
+      }
+    }
+  },
+  methods: {
+    openStorage () {
+      return JSON.parse(localStorage.getItem('form'))
+    },
+    saveStorage (form) {
+      localStorage.setItem('form', JSON.stringify(form))
+    },
+    updateForm (input, value) {
+      this.form[input] = value
+
+      let storedForm = this.openStorage()
+      if (!storedForm) storedForm = {}
+
+      storedForm[input] = value
+      this.saveStorage(storedForm)
+    }
+  },
+  created () {
+    const storedForm = this.openStorage()
+    if (storedForm) {
+      this.form = storedForm
     }
   }
 }
