2f1839b5cdd572fc373695af0ccf6f9dc54bb24b
Marina Mosti <marina@mosti.com.mx>
2019-11-18 22:00:27 -0600

    Set up base form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,50 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <hr />
+    <form>
+      <label for="firstName">First Name</label>
+      <input type="text" id="firstName" v-model="firstName" />
+
+      <label for="lastName">Last Name</label>
+      <input type="text" id="lastName" v-model="lastName" />
+
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model="email" />
+
+      <label for="framework">Favorite Framework</label>
+      <select id="framework" v-model="framework">
+        <option value="vue">Vue</option>
+        <option value="stillvue">Vue!</option>
+        <option value="vuevuevue">Vue :D</option>
+        <option value="okfine">Other</option>
+      </select>
+
+      <label for="extras">Heres an extra space to describe why you love Vue</label>
+      <textarea id="extras" v-model="extras" rows="5"></textarea>
+
+      <label>
+        <input type="checkbox" v-model="spam" />
+        I want all the spams
+      </label>
+
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      firstName: '',
+      lastName: '',
+      email: '',
+      framework: 'vue',
+      extras: '',
+      spam: ''
+    }
   }
 }
 </script>
@@ -24,5 +57,21 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+form {
+  width: 300px;
+  margin: 0 auto;
+  text-align: left
+}
+
+label, input, select, textarea {
+  display: block;
+  width: 100%;
+
+  input {
+    display: inline;
+    width: auto;
+  }
 }
 </style>
